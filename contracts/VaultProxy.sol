ffa4cd04cdd925465263b1a12f14090c57cc12a4
Nuthan Prasad <nuthan1159@gmail.com>
2023-04-16 11:03:18 +0530

    proxy changed

--- contracts/VaultProxy.sol
+++ contracts/VaultProxy.sol
@@ -4,12 +4,12 @@ pragma solidity 0.8.18;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
-contract VaultProxy is UUPSUpgradeable, OwnableUpgradeable {
-    constructor(address initialImpl) initializer{
+contract VaultProxy is  ERC1967Proxy, OwnableUpgradeable  {
+    constructor(address initialImpl) ERC1967Proxy(initialImpl, "") initializer{
         _upgradeTo(initialImpl);
         __Ownable_init();
     }
 
-   function _authorizeUpgrade(address) internal override onlyOwner {}
 }
--- src/contracts.js
+++ src/contracts.js
@@ -1,5 +1,5 @@
 const ADDRESSES = {
-    "VaultFactory" : "0x39331C17e714f1505dB65ea1C56212e5b1CC8718",
+    "VaultFactory" : "0x60A902f3Ce5c9D394DacBCf6566624F901eaEee7",
     "Keeper" : "0xD5fba05dE4b2d303D03052e8aFbF31a767Bd908e",
     "Governor" : "0xD5fba05dE4b2d303D03052e8aFbF31a767Bd908e",
     "Vault" : "0x5EE69845af2Ba394F460b4AD39963e7625f66EAA",
